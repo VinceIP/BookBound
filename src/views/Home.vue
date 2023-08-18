9f4544ad21b045ed000d1b55b406ded68ac0b954
VinceIP <vinceip532@gmail.com>
2023-08-18 15:07:47 -0400

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,7 +59,7 @@
       <div class="product_details">
         <div class="info_box">
           <h1>
-            Our family reading program, including parents, is more than just reading - it's a relationship builder. It
+            Our family reading program is more than just reading - it's a relationship builder. It
             fosters love for books and literacy in children while allowing parents to bond over stories. With access to
             age-appropriate content, parents can be confident their children are exploring suitable material. This program
             is not only a gateway to the wonders of storytelling but also a catalyst for quality family time and lifelong
@@ -98,109 +98,221 @@ export default {
 </script>
 
 <style scoped>
+.content {
+  margin-left: 15vw;
+  margin-right: 15vw;
+}
+
+.home {
+  padding: 1vw;
+  background: var(--background-color);
+}
+
+.title {
+  display: flex;
+  font-family: var(--header-font);
+  background-color: var(--background-color-green-mute);
+  color: var(--font-color);
+  align-items: center;
+  justify-content: center;
+  margin: 30px 15px 30px 15px;
+  border-radius: 30%;
+  height: 25vh;
+  box-shadow: var(--panel-shadow);
+}
+
+.title h1 {
+  font-size: 6.5vh;
+  text-shadow: 15px 15px 10px rgba(0, 0, 0, 0.329);
+  text-align: center;
+}
+
+.title img {
+  width: 20vw;
+  filter: drop-shadow(25px 35px 8px rgba(0, 0, 0, 0.329));
+}
+
 #info_box_header_container {
   display: flex;
-  margin-top: 5rem;
-  margin-bottom: 5rem;
+  margin-top: 10vh;
+  margin-bottom: 10vh;
   align-items: center;
   justify-content: center;
 }
 
 #info_box_header {
-  width: 50%;
+  width: 30%;
   background: rgb(115, 147, 126);
   border: 1px;
   border-color: rgb(55, 77, 62);
   border-style: solid;
   border-radius: 40px;
-  padding: 10px;
+  padding: 1vh;
   box-shadow: var(--panel-shadow);
 }
 
 #info_box_header h1 {
   font-family: var(--header-font);
-  font-size: 28px;
+  font-size: 4vh;
   text-align: center;
   color: var(--font-color);
 }
 
-/* -- Mobile phones -- */
+.product_details {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  column-gap: 2vw;
+  padding: 1rem;
+  margin-top:1.5cm;
+  margin-bottom: 1.5cm;
+  width: 100%;
+  align-items: center;
+  justify-items: center;
+}
+
+.info_box {
+  height: fit-content;
+  width: 100%;
+  background: rgb(115, 147, 126);
+  border: 2px;
+  border-color: rgb(55, 77, 62);
+  border-style: solid;
+  border-radius: 80px;
+  padding: 25px;
+  box-shadow: var(--panel-shadow);
+}
+
+.info_box h1 {
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+}
+
+.book_covers {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
+  grid-template-rows: auto;
+  gap: 5rem;
+  width: 90%;
+}
+
+.book_covers .book_cover {
+  filter: drop-shadow(15px 15px 8px rgba(0, 0, 0, 0.329));
+  width: 8rem;
+  height: auto;
+}
+
+/* -- Mobile large -- */
 @media(max-width: 916px) {
   .home {
     padding: 0.5rem;
-    background: var(--background-color);
+  }
+
+  .content {
+    margin-left: 0rem;
+    margin-right: 0rem;
   }
 
   .title {
-    display: flex;
-    background-color: var(--background-color-green-mute);
-    color: var(--font-color);
-    align-items: center;
-    justify-items: center;
-    border-radius: 50%;
-    box-shadow: var(--panel-shadow);
     height: 15rem;
   }
 
   .title h1 {
-    font-family: var(--header-font);
     font-size: 32px;
-    text-shadow: 15px 15px 10px rgba(0, 0, 0, 0.329);
     text-align: right;
-    padding:1rem;
+    padding: 1rem;
   }
 
   .title img {
     width: 350px;
-    filter: drop-shadow(25px 35px 8px rgba(0, 0, 0, 0.329));
-  }
-
-  .content {
-    margin-left: 1rem;
-    margin-right: 1rem;
   }
 
   .product_details {
-    display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: auto;
-    row-gap: 2rem;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
+    row-gap: 5rem;
   }
 
   .info_box {
     height: fit-content;
-    width: 100%;
-    background: rgb(115, 147, 126);
-    border: 2px;
-    border-color: rgb(55, 77, 62);
-    border-style: solid;
-    border-radius: 80px;
-    box-shadow: var(--panel-shadow);
-
+    width: 40rem;
+    padding: 1.25rem;
   }
 
   .info_box h1 {
-    font-family: "Montserrat", sans-serif;
     font-size: 16px;
-    padding: 25px
   }
 
   .book_covers {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    gap: 2rem;
+    gap: 5rem;
     width: 100%;
-    justify-items: center;
-    justify-content: center;
-    margin-bottom: 3rem;
   }
 
   .book_covers .book_cover {
-    filter: drop-shadow(15px 15px 8px rgba(0, 0, 0, 0.329));
-    width: 8rem;
+    width: 5rem;
+    height: auto;
+  }
+
+}
+
+/* -- Mobile small -- */
+@media(max-width: 713px) {
+  .home {
+    padding: 0rem;
+  }
+
+  .content {
+    margin-left: 0rem;
+    margin-right: 0rem;
+  }
+
+  .title {
+    height: 15rem;
+  }
+
+  .title h1 {
+    font-size: 30px;
+    text-align: left;
+    padding: 0rem;
+  }
+
+  .title img {
+    width: 250px;
+  }
+
+  .product_details {
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    column-gap: 0rem;
+    padding: 0rem;
+    margin-bottom: 0rem;
+    width: 85vw;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .info_box {
+    height: fit-content;
+    width: inherit;
+    padding: 1.25rem;
+  }
+
+  .info_box h1 {
+    font-size: 16px;
+  }
+
+  .book_covers {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
+    width: 100%;
+    
+  }
+
+  .book_covers .book_cover {
+    width: 5rem;
     height: auto;
   }
 
@@ -208,131 +320,7 @@ export default {
 
 /*-- Desktop --*/
 @media(min-width: 916px) {
-  .content {
-    margin-left: 12rem;
-    margin-right: 12rem;
-  }
 
-
-  .home {
-    padding: 0.5rem;
-    background: var(--background-color);
-  }
-
-  .title {
-    display: flex;
-    font-family: var(--header-font);
-    background-color: var(--background-color-green-mute);
-    color: var(--font-color);
-    align-items: center;
-    margin: 30px 15px 30px 15px;
-    border-radius: 50%;
-    height: 250px;
-    box-shadow: var(--panel-shadow);
-  }
-
-  .title h1 {
-    font-size: 58px;
-    text-shadow: 15px 15px 10px rgba(0, 0, 0, 0.329);
-    text-align: center;
-    margin-left: 30%;
-  }
-
-  .title img {
-    width: 400px;
-    filter: drop-shadow(25px 35px 8px rgba(0, 0, 0, 0.329));
-  }
-
-  logo-slider {
-    display: flex;
-    border: 2px solid rgb(0, 0, 0);
-    background: rgb(255, 237, 200);
-
-    box-shadow: var(--panel-shadow);
-    padding: 10px;
-    height: 325px;
-    overflow: hidden;
-    width: 70%;
-    position: relative;
-    margin: 0 auto 50px;
-  }
-
-  logo-slider:hover .slideshow {
-    animation-play-state: paused;
-  }
-
-  .slideshow {
-    display: flex;
-    animation: marquee 40s linear infinite;
-    justify-content: space-evenly;
-  }
-
-  #slides {
-    width: 45rem;
-    height: auto;
-    padding: 0rem 1rem 0rem 1rem;
-
-  }
-
-  @keyframes marquee {
-    0% {
-      transform: translateX(0%)
-    }
-
-    100% {
-      transform: translateX(-50%)
-    }
-  }
-
-
-
-
-
-  .product_details {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: auto;
-    column-gap: 5rem;
-    padding: 1rem;
-    width: 100%;
-    align-items: center;
-  }
-
-  .info_box {
-    height: fit-content;
-    width: 80%;
-    align-self: center;
-    justify-self: center;
-    background: rgb(115, 147, 126);
-    border: 2px;
-    border-color: rgb(55, 77, 62);
-    border-style: solid;
-    border-radius: 80px;
-    padding: 25px;
-    box-shadow: var(--panel-shadow);
-
-  }
-
-  .info_box h1 {
-    font-family: "Montserrat", sans-serif;
-    font-size: 16px;
-  }
-
-  .book_covers {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(50px, 1fr));
-    grid-template-rows: auto;
-    gap: 1rem;
-    width: 90%;
-    justify-items: center;
-    align-items: center;
-  }
-
-  .book_covers .book_cover {
-    filter: drop-shadow(15px 15px 8px rgba(0, 0, 0, 0.329));
-    width: 10rem;
-    height: auto;
-  }
 
   .product_details:nth-child(odd) .info_box {
     order: 1;
