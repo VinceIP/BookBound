bf92eaee258261ac4b154decaae488b4f8591b55
VinceIP <vinceip532@gmail.com>
2023-08-19 09:49:24 -0400

    More mobile updates

--- dist/js/app.bb593b70.js
+++ dist/js/app.bb593b70.js
@@ -2,7 +2,7 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 8498:
+/***/ 2016:
 /***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
 
 
@@ -136,13 +136,13 @@ var es_array_some = __webpack_require__(5212);
 var es_object_to_string = __webpack_require__(1539);
 // EXTERNAL MODULE: ./node_modules/vue-router/dist/vue-router.esm.js
 var vue_router_esm = __webpack_require__(8345);
-;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/Home.vue?vue&type=template&id=53d68934&scoped=true&
-var Homevue_type_template_id_53d68934_scoped_true_render = function render() {
+;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/templateLoader.js??ruleSet[1].rules[3]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/Home.vue?vue&type=template&id=61cb95fa&scoped=true&
+var Homevue_type_template_id_61cb95fa_scoped_true_render = function render() {
   var _vm = this,
     _c = _vm._self._c;
   return _vm._m(0);
 };
-var Homevue_type_template_id_53d68934_scoped_true_staticRenderFns = [function () {
+var Homevue_type_template_id_61cb95fa_scoped_true_staticRenderFns = [function () {
   var _vm = this,
     _c = _vm._self._c;
   return _c('div', {
@@ -286,7 +286,7 @@ var Homevue_type_template_id_53d68934_scoped_true_staticRenderFns = [function ()
   })])])])])]);
 }];
 
-;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=template&id=53d68934&scoped=true&
+;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=template&id=61cb95fa&scoped=true&
 
 ;// CONCATENATED MODULE: ./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib/index.js??clonedRuleSet-40.use[1]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/Home.vue?vue&type=script&lang=js&
 /* harmony default export */ const Homevue_type_script_lang_js_ = ({
@@ -294,10 +294,10 @@ var Homevue_type_template_id_53d68934_scoped_true_staticRenderFns = [function ()
 });
 ;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=script&lang=js&
  /* harmony default export */ const views_Homevue_type_script_lang_js_ = (Homevue_type_script_lang_js_); 
-;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-12.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-12.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-12.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/Home.vue?vue&type=style&index=0&id=53d68934&prod&scoped=true&lang=css&
+;// CONCATENATED MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-12.use[0]!./node_modules/css-loader/dist/cjs.js??clonedRuleSet-12.use[1]!./node_modules/@vue/vue-loader-v15/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-12.use[2]!./node_modules/@vue/vue-loader-v15/lib/index.js??vue-loader-options!./src/views/Home.vue?vue&type=style&index=0&id=61cb95fa&prod&scoped=true&lang=css&
 // extracted by mini-css-extract-plugin
 
-;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=style&index=0&id=53d68934&prod&scoped=true&lang=css&
+;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=style&index=0&id=61cb95fa&prod&scoped=true&lang=css&
 
 ;// CONCATENATED MODULE: ./src/views/Home.vue
 
@@ -310,11 +310,11 @@ var Homevue_type_template_id_53d68934_scoped_true_staticRenderFns = [function ()
 
 var Home_component = (0,componentNormalizer/* default */.Z)(
   views_Homevue_type_script_lang_js_,
-  Homevue_type_template_id_53d68934_scoped_true_render,
-  Homevue_type_template_id_53d68934_scoped_true_staticRenderFns,
+  Homevue_type_template_id_61cb95fa_scoped_true_render,
+  Homevue_type_template_id_61cb95fa_scoped_true_staticRenderFns,
   false,
   null,
-  "53d68934",
+  "61cb95fa",
   null
   
 )
@@ -3660,9 +3660,9 @@ module.exports = __webpack_require__.p + "img/sharpened-transparent-logo.5864fba
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [998], () => (__webpack_require__(8498)))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [998], () => (__webpack_require__(2016)))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=app.b2849878.js.map
+//# sourceMappingURL=app.bb593b70.js.map
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <div class="page_header">
       <h1>BookBound: Discover the Power of Stories Together!</h1>
-      <img class="floating" src="../assets/images/sharpened-transparent-logo.png" />
+      <img
+        class="floating"
+        src="../assets/images/sharpened-transparent-logo.png"
+      />
     </div>
     <!-- <logo-slider>
       <div class="slideshow">
@@ -17,74 +20,147 @@
       </div>
     </logo-slider> -->
     <div class="content">
-        <div class="rounded_panel" id="header_panel">
-          <h1>What is BookBound?</h1>
-        </div>
+      <div class="rounded_panel" id="header_panel">
+        <h1>What is BookBound?</h1>
+      </div>
       <div class="product_details_container">
-      <div class="product_details">
-        <div class="rounded_panel">
-          <h1>
-            Dive into a world of reading with your family through an incredible program designed for kids and parents
-            alike. Explore captivating stories, take part in exciting reading contests, and win prizes. It's more than a
-            reading challenge; it's a thrilling adventure that bonds families and creates lasting memories. Embark on this
-            extraordinary journey where stories come to life, and shared joy awaits!
-          </h1>
+        <div class="product_details">
+          <div class="rounded_panel">
+            <h1>
+              Dive into a world of reading with your family through an
+              incredible program designed for kids and parents alike. Explore
+              captivating stories, take part in exciting reading contests, and
+              win prizes. It's more than a reading challenge; it's a thrilling
+              adventure that bonds families and creates lasting memories. Embark
+              on this extraordinary journey where stories come to life, and
+              shared joy awaits!
+            </h1>
+          </div>
+          <div class="book_covers">
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/winniethepooh.jpg"
+              alt="winnie the pooh"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/aliceinwonderland.jpg"
+              alt="alice in wonderland"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/dracula.jpg"
+              alt="dracula"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/anneofgreengables.jpg"
+              alt="anne of green gables"
+            />
+          </div>
         </div>
-        <div class="book_covers">
-          <img class="book_cover" src="../assets/images/book covers/winniethepooh.jpg" alt="winnie the pooh" />
-          <img class="book_cover" src="../assets/images/book covers/aliceinwonderland.jpg" alt="alice in wonderland" />
-          <img class="book_cover" src="../assets/images/book covers/dracula.jpg" alt="dracula" />
-          <img class="book_cover" src="../assets/images/book covers/anneofgreengables.jpg" alt="anne of green gables" />
+        <div class="product_details">
+          <div class="rounded_panel">
+            <h1>
+              Say goodbye to disorganized reading records. Our family reading
+              tracking website is a centralized hub where each family member can
+              manage their profiles, curate reading lists, and track progress.
+              This interactive platform turns reading into a shared adventure,
+              fostering both camaraderie and friendly competition. Join the
+              journey and transform reading into a family bonding experience!
+            </h1>
+          </div>
+          <div class="book_covers">
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/frankenstein.jpg"
+              alt="frankie"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/greatgatbsy.jpg"
+              alt="great gatbsy"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/GUEST_a0eb4016-52b3-4bbe-a1b9-8c208c81bb42.jpg"
+              alt="wizard of oz"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/gulliverstravels.jpg"
+              alt="gulliver's traavels"
+            />
+          </div>
         </div>
-      </div>
-      <div class="product_details">
-        <div class="rounded_panel">
-          <h1>
-            Say goodbye to disorganized reading records. Our family reading tracking website is a centralized hub where
-            each family member can manage their profiles, curate reading lists, and track progress. This interactive
-            platform turns reading into a shared adventure, fostering both camaraderie and friendly competition. Join the
-            journey and transform reading into a family bonding experience!
-          </h1>
+        <div class="product_details">
+          <div class="rounded_panel">
+            <h1>
+              Our family reading program is more than just reading - it's a
+              relationship builder. It fosters love for books and literacy in
+              children while allowing parents to bond over stories. With access
+              to age-appropriate content, parents can be confident their
+              children are exploring suitable material. This program is not only
+              a gateway to the wonders of storytelling but also a catalyst for
+              quality family time and lifelong learning. Embrace reading as a
+              family, and strengthen your connections through the joy of
+              stories!
+            </h1>
+          </div>
+          <div class="book_covers">
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/jekyllhyde.jpg"
+              alt=""
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/journeytocenterofearth.jpg"
+              alt=""
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/littlewomen.jpg"
+              alt=""
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/peterrabbit.jpg"
+              alt=""
+            />
+          </div>
         </div>
-        <div class="book_covers">
-          <img class="book_cover" src="../assets/images/book covers/frankenstein.jpg" alt="frankie" />
-          <img class="book_cover" src="../assets/images/book covers/greatgatbsy.jpg" alt="great gatbsy" />
-          <img class="book_cover" src="../assets/images/book covers/GUEST_a0eb4016-52b3-4bbe-a1b9-8c208c81bb42.jpg"
-            alt="wizard of oz" />
-          <img class="book_cover" src="../assets/images/book covers/gulliverstravels.jpg" alt="gulliver's traavels" />
+        <div class="product_details">
+          <div class="rounded_panel">
+            <h1>
+              Join us for epic tales, incredible connections, and the thrill of
+              reading. Embark on this adventure and explore the wonderful world
+              of books with us!
+            </h1>
+          </div>
+          <div class="book_covers">
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/pictureofdoriangray.jpg"
+              alt="dorian gray"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/theOdyssey.jpg"
+              alt="odyessy"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/velvateenrabbit.jpg"
+              alt="velvateen rabbit"
+            />
+            <img
+              class="book_cover"
+              src="../assets/images/book covers/wutheringheights.jpg"
+              alt="wuthering heights"
+            />
+          </div>
         </div>
-      </div>
-      <div class="product_details">
-        <div class="rounded_panel">
-          <h1>
-            Our family reading program is more than just reading - it's a relationship builder. It
-            fosters love for books and literacy in children while allowing parents to bond over stories. With access to
-            age-appropriate content, parents can be confident their children are exploring suitable material. This program
-            is not only a gateway to the wonders of storytelling but also a catalyst for quality family time and lifelong
-            learning. Embrace reading as a family, and strengthen your connections through the joy of stories!
-          </h1>
-        </div>
-        <div class="book_covers">
-          <img class="book_cover" src="../assets/images/book covers/jekyllhyde.jpg" alt="" />
-          <img class="book_cover" src="../assets/images/book covers/journeytocenterofearth.jpg" alt="" />
-          <img class="book_cover" src="../assets/images/book covers/littlewomen.jpg" alt="" />
-          <img class="book_cover" src="../assets/images/book covers/peterrabbit.jpg" alt="" />
-        </div>
-      </div>
-      <div class="product_details">
-        <div class="rounded_panel">
-          <h1>
-            Join us for epic tales, incredible connections, and the thrill of reading. Embark on this adventure and
-            explore the wonderful world of books with us!
-          </h1>
-        </div>
-        <div class="book_covers">
-          <img class="book_cover" src="../assets/images/book covers/pictureofdoriangray.jpg" alt="dorian gray" />
-          <img class="book_cover" src="../assets/images/book covers/theOdyssey.jpg" alt="odyessy" />
-          <img class="book_cover" src="../assets/images/book covers/velvateenrabbit.jpg" alt="velvateen rabbit" />
-          <img class="book_cover" src="../assets/images/book covers/wutheringheights.jpg" alt="wuthering heights" />
-        </div>
-      </div>
       </div>
     </div>
   </div>
@@ -102,7 +178,7 @@ export default {
 }
 
 #header_panel {
-  width:550px;
+  width: 550px;
 }
 
 #header_panel h1 {
@@ -112,8 +188,8 @@ export default {
   color: var(--font-color);
 }
 
-.product_details_container{
-  display:grid;
+.product_details_container {
+  display: grid;
   grid-template-columns: 1fr;
   row-gap: 30px;
 }
@@ -125,18 +201,18 @@ export default {
   align-items: center;
   justify-items: center;
   background-color: var(--background-color-popout);
-  padding:20px;
+  padding: 20px;
   box-shadow: var(--panel-shadow);
 }
 
-.rounded_panel{
-  width:700px;
+.rounded_panel {
+  width: 700px;
 }
 
 .rounded_panel h1 {
   font-family: var(--main-font);
   font-size: 16px;
-  padding:10px;
+  padding: 10px;
 }
 
 .book_covers {
@@ -154,48 +230,96 @@ export default {
 }
 
 /*-- Mobile -- */
-@media(max-width:500px){
-
-
-  .page_header{
+@media (max-width: 500px) {
+  .page_header {
   }
-  .page_header h1{
-    padding:3px;
+  .page_header h1 {
+    padding: 3px;
   }
   .page_header img {
-        position: absolute;
-        top:90px;
-        left:-40px;
-        width: 325px;
-        height: auto;
-        filter: drop-shadow(25px 35px 8px rgba(0, 0, 0, 0.329));
-      }
+    position: absolute;
+    top: 90px;
+    left: -40px;
+    width: 325px;
+    height: auto;
+    filter: drop-shadow(25px 35px 8px rgba(0, 0, 0, 0.329));
+  }
 
-      .floating { 
-        margin-left: 0px;
-        margin-top: 0px;
+  .floating {
+    margin-left: 0px;
+    margin-top: 0px;
+  }
+
+  @keyframes floating {
+    0% {
+      transform: translate(0%, 30px);
     }
-     
-    @keyframes floating {
-        0% { transform: translate(0%,  30px); }
-        50%  { transform: translate(0%, 45px); }
-        100%   { transform: translate(0%, 30px); }   
+    50% {
+      transform: translate(0%, 45px);
     }
-    .content{
+    100% {
+      transform: translate(0%, 30px);
+    }
+  }
+  .content {
     position: relative;
-    top:250px;
+    margin-left: 1vw;
+    margin-right: auto;
+    top: 290px;
+    width: 90vw;
+  }
+
+  #header_panel {
+    width: 70vw;
+  }
+  #header_panel h1 {
+    font-size: 22px;
+  }
+
+  .product_details_container {
+    width: 100%;
+    justify-self: flex-start;
+    align-self: flex-start;
+  }
+
+  .product_details {
+    width: 86vw;
+    grid-template-columns: 1fr;
+    padding: 15px;
+  }
+
+  .product_details .rounded_panel{
+    height: fit-content;
+    padding: 9px;
+    margin: 5px 0px 15px 0px;
+    width:100%;
+}
+
+  .rounded_panel h1 {
+    font-size: 12px;
+    padding: 3px;
+  }
+
+  .book_covers {
+    grid-template-columns: 1fr 1fr;
+    gap: 15px;
+  }
+
+  .book_covers .book_cover {
+    width: 80px;
+    align-self: center;
+    justify-self: center;
   }
 }
 
 /*-- Desktop --*/
-@media(min-width: 916px) {
+@media (min-width: 916px) {
   .product_details:nth-child(odd) .rounded_panel {
     order: 1;
   }
 
   .product_details:nth-child(odd) .book_covers {
     order: 2;
-    
   }
 
   .product_details:nth-child(even) .rounded_panel {
